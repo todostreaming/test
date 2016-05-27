225b979a52f83f381890db316f88a72010ac5c34
todostreaming <info@todostreaming.es>
2016-05-27 22:56:14 +0200

    fifo on structs

--- types/main.go
+++ types/main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
-	"github.com/todostreaming/fifo"
 	"fmt"
+	"github.com/todostreaming/fifo"
 )
 
-func main(){
-	ts_queue := fifo.NewQueue()
-	fmt.Printf("1)Len=%d\n",ts_queue.Len())
-	ts_queue.Add("segment1.ts")
-	ts_queue.Add("segment2.ts")
-	ts_queue.Add("segment3.ts")
-
-	for q := ts_queue.Next(); q != nil; q = ts_queue.Next() {
-		fmt.Println(q)
-	}
-//	total := ts_queue.Len()
-//	for q:=0; q < total; q++ {
-//		fmt.Println(ts_queue.Next())
-//	}
+type Segment struct {
+	Name string
+	Dur  float64
 }
 
+func main() {
+	ts_queue := fifo.NewQueue()
+	ts_queue.Add(&Segment{"stream1.ts", 10.56})
+	ts_queue.Add(&Segment{"stream2.ts", 9.02})
+	ts_queue.Add(&Segment{"stream3.ts", 11.00})
+
+	for q := ts_queue.Next(); q != nil; q = ts_queue.Next() {
+		p := q.(*Segment)
+		fmt.Printf("TS: %s \t %.2f\n", p.Name, p.Dur)
+	}
+
+	/*
+		fmt.Printf("1)Len=%d\n",ts_queue.Len())
+		ts_queue.Add("segment1.ts")
+		ts_queue.Add("segment2.ts")
+		ts_queue.Add("segment3.ts")
+
+		for q := ts_queue.Next(); q != nil; q = ts_queue.Next() {
+			fmt.Println(q)
+		}
+	*/
+	//	total := ts_queue.Len()
+	//	for q:=0; q < total; q++ {
+	//		fmt.Println(ts_queue.Next())
+	//	}
+}
